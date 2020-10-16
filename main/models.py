12267b3b11c77b4fd7fafca1cf52c2656d89de52
Filip Suster <filip.suster@t-mobile.cz>
2020-10-16 12:49:35 +0200

    MODEL change: Create the initial models/migrations

--- main/models.py
+++ main/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=32, primary_key=True)
+    password = models.CharField(max_length=32)  # TODO: Of course dude, this needs to be changed after testing
+
+    name = models.CharField(max_length=32)
+    surname = models.CharField(max_length=32)
+
+
+class Place(models.Model):
+    username = models.ForeignKey('User', on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    default_lunchtime = models.SmallIntegerField(default=30)  # minutes
+
+
+class Action(models.Model):
+    place = models.ForeignKey('Place', on_delete=models.CASCADE)
+    type = models.CharField(max_length=32)
+    timestamp = models.DateTimeField()
