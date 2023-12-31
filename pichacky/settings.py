8c313513727615671af7bd706d11f8e7f709a1cf
Filip Suster <sustefil@gmail.com>
2023-12-31 14:24:17 +0100

    Add tests and .env to docker compose

--- pichacky/settings.py
+++ pichacky/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'NECUM'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv('DEBUG', 'false').lower() in ('1', 'true') else False
 
 ALLOWED_HOSTS = []
 
