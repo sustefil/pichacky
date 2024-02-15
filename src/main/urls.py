7fc131a8de7690be361640b3d221567bbc5baa36
Filip Suster <sustefil@gmail.com>
2024-02-15 13:59:20 +0100

    Add testing endpoint for getting users

--- src/main/urls.py
+++ src/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('users/', views.get_users, name='get_users'),
     path('err-test/', views.err_test, name='err_test'),
 ]
--- src/main/views.py
+++ src/main/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from src.main.models import User
 
 
 def index(request, extra_msg=None):
-    return HttpResponse(f"CI TEST-ULTIMATE. Hello, world. You're at the Pichacky index. {extra_msg}")
+    return HttpResponse(f"Hello, world. You're at the Pichacky index. {extra_msg}")
 
 
 def err_test(request, extra_msg=None):
     return HttpResponse("Testing errors", status=500)
+
+
+def get_users(request, extra_msg=None):
+    return HttpResponse('<br>'.join([str(x) for x in User.objects.all().values_list(named=True)]))
