eaf44ac97a91597bc569ca9b0053c2d67d2fb9cd
Filip Suster <sustefil@gmail.com>
2024-02-15 15:15:41 +0100

    Add computation endpoint

--- src/main/urls.py
+++ src/main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('users/', views.get_users, name='get_users'),
     path('err-test/', views.err_test, name='err_test'),
+    path('compute/this-is-a-very-top-secret-endpoint-that-makes-the-world-burn/<int:exponent>/', views.compute, name='compute'),
 ]
--- src/main/views.py
+++ src/main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from src.main.models import User
+from time import sleep, perf_counter
+from math import sqrt
 
 
 def index(request, extra_msg=None):
@@ -14,3 +16,15 @@ def err_test(request, extra_msg=None):
 
 def get_users(request, extra_msg=None):
     return HttpResponse('<br>'.join([str(x) for x in User.objects.all().values_list(named=True)]))
+
+
+def compute(request, exponent, extra_msg=None):
+    start = perf_counter()
+
+    c = 0
+    for i in range(10 ** exponent):
+        c += sqrt(i)
+
+    end = perf_counter()
+
+    return HttpResponse(f'Computation took {end - start} seconds')
